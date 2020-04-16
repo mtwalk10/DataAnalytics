5badbd33a1b4f8a2ed67edeeda1219171a585a50
mtwalk10 <54559718+mtwalk10@users.noreply.github.com>
2020-04-16 11:45:49 -0400

    Additional Data Manipulation Queries

--- MotorcycleDataPresentation.py
+++ MotorcycleDataPresentation.py
@@ -74,3 +74,69 @@ GROUP BY Make
 make_query(query)
 
 
+# - Data Manipulation - Finding the Three Best Selling Makes (By Number of Sales)
+
+query = """
+SELECT COUNT(Price) AS "Number of Sales", Make 
+FROM Sales
+GROUP BY Make
+ORDER BY COUNT(Price) DESC
+LIMIT 3;
+"""
+make_query(query)
+
+
+# - Data Manipulation - Finding the Three Best Selling Makes (By Total Value of Sales)
+
+query = """
+SELECT '$' || SUM(CAST(REPLACE(REPLACE(Price, '$',''), ',','') AS int)) AS "Total Sales by Make", Make
+FROM Sales
+GROUP BY Make
+ORDER BY SUM(CAST(REPLACE(REPLACE(Price, '$',''), ',','') AS int)) DESC
+LIMIT 3;
+"""
+make_query(query)
+
+
+# - Data Manipulation - Finding the Three Highest Individual Sales
+query = """
+SELECT '$' || CAST(REPLACE(REPLACE(Price, '$',''), ',','') AS int) AS "Sale Price", Make, Model_Year AS "Year", 
+Mileage, Exterior_Color AS "COLOR"
+FROM Sales
+GROUP BY Make
+ORDER BY CAST(REPLACE(REPLACE(Price, '$',''), ',','') AS int) DESC
+LIMIT 3;
+"""
+make_query(query)
+
+
+# - Data Manipulation - Finding the Three Oldest Bikes Sold
+query = """
+SELECT '$' || CAST(REPLACE(REPLACE(Price, '$',''), ',','') AS int) AS "Sale Price", Make, Model_Year AS "Year"
+FROM Sales
+ORDER BY Model_Year
+LIMIT 3
+"""
+make_query(query)
+
+
+# - Data Manipulation - Finding the Three Most Common Colors of Bike Sold
+query = """
+SELECT COUNT(Exterior_Color) AS "Number of Bikes Sold", Exterior_Color AS "Color"
+FROM Sales
+GROUP BY Exterior_Color
+ORDER BY COUNT(Exterior_Color) DESC
+LIMIT 3
+"""
+make_query(query)
+
+
+
+# - Data Manipulation - Finding the Most Popular Model for each Make
+query = """
+SELECT Make, Model AS "Most Popular Model", COUNT(Model) AS "Number of Bikes Sold"
+FROM Sales
+GROUP BY Make
+"""
+make_query(query)
+
